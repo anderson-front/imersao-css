112140b4bf4fb120896a3a5f13d7cf282c7b448e
Anderson Santos <anderon.santos.silva@live.com>
2020-07-18 13:58:31 -0300

    Primeira versão adaptada para oferecer serviços web

--- script.js
+++ script.js
@@ -4,25 +4,21 @@
 
 function carregar(){
   var img = document.getElementById('foto-periodo')
-  
   var data = new Date()
+  var hora = data.getHours()
 
-  var hora = data.getHours
 
-  if(hora >=0 && hora <= 11){
-    img.src = "img/manha.png"
-
- }else if(hora >= 12 && hora <=18){
-   
-   img.src = "img/noite.png"
-//    document.body.style.background ='#e2c019'
-                                   
- }else{
-   img.src = "img/noite.png"
+  if(hora >=0 && hora < 12){
+     img.src = "img/manha.png";
+  }
+ else if(hora >= 12 && hora <=18){
+     img.src = "img/tarde.png";
+ }
+ else{
+    img.src = "img/noite.png";
 //    document.body.style.background ='#515154'
  
  }
 
 
-  
 }
